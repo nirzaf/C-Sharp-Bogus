158ffb54a4a0bc7d256be595c9501ad443e318bc
Fazrin <mfmfazrin1986@gmail.com>
2022-08-17 18:39:11 +0300

    Compiled

---
+++ EmployeeDB/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeDB.Models;
+
+public class Employee
+{
+    [Key] public long EmployeeId { get; set;}
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Gender { get; set; }
+    public int Age { get; set; }
+    public float Salary { get; set; }
+}
